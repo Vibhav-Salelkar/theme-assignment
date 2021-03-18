56c89a273de0cbe3c017cd7cddd629570585c3e6
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-18 14:50:01 +0530

    feat: blog posts stylings

--- page-blog.php
+++ page-blog.php
@@ -20,48 +20,24 @@ get_header();
 
 
 <div class="blog-custom">
- <div class="blog-posts">
-      <h2 class="blog-custom-title">LET'S BLOG</h2>
-      <hr>
-      <div class="all-posts">
-        <?php
-        $args = array( 'posts_per_page' => 3 );
-
-        $the_query = new WP_Query( $args );
-
-        if ( $the_query->have_posts() ) :
-            while ( $the_query->have_posts() ) : $the_query->the_post();
-              ?>
-              <div class="single_post" style="margin-bottom: 1.2rem;">
-                <h3><?php the_title(); ?></h3>
-                <div class="">
-                  <div class="single_post-meta">
-                    <div class="single_post-meta-author">
-                      <h4>by <span><?php the_author(); ?></span></h4>
-                      <p style="margin:0px 0.8rem;"><?php the_time(); ?></p>
-                    </div>
-                    <p class="post-comments"><a style="text-decoration: none;" href="<?php echo comments_link(); ?>"><?php echo get_comments_number();?> comments</a></p>
-                  </div>
-                  <div class="single_post-content">
-                    <p><?php the_excerpt(); ?></p>
-                    <a href=" <?php echo get_post_permalink(); ?>">Read more</a>
-                  </div>
-               </div>
-             </div>
-              <?php
-            endwhile;
-        else:
-            _e( 'Sorry, no posts matched your criteria.', 'wb_domain' );
-        endif;
-        wp_reset_postdata();
-        ?>
-      </div>
- </div>
-    <div class="blog-sidebar">
-      <?php get_sidebar(); ?>
-    </div>
+     <div class="blog-posts">
+          <h2 class="blog-custom-title">LET'S BLOG</h2>
+          <hr>
+          <div class="all-posts">
+            <?php get_template_part( 'template-parts/blog-posts', get_post_type() ); ?>
+          </div>
+     </div>
+     <div class="blog-sidebar">
+       <?php get_sidebar(); ?>
+     </div>
 </div>
-
+<div class="paginate">
+  <span>1</span>
+  <span>2</span>
+  <span>3</span>
+  <span class="dashicons dashicons-controls-play"></span>
+</div>
+<hr class="line-break">
 <?php
 
 get_footer();
@@ -69,7 +45,43 @@ get_footer();
 
 
 /*
+<div class="">
+  <img class="grid_container-item grid_container-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-2.png">
+</div>
+**/
 
+/*
+<?php
+$args = array( 'posts_per_page' => 3 );
+
+$the_query = new WP_Query( $args );
+
+if ( $the_query->have_posts() ) :
+    while ( $the_query->have_posts() ) : $the_query->the_post();
+      ?>
+      <div class="single_post" style="margin-bottom: 1.2rem;">
+        <h3><?php the_title(); ?></h3>
+        <div class="">
+          <div class="single_post-meta">
+            <div class="single_post-meta-author">
+              <h4>by <span><?php the_author(); ?></span></h4>
+              <p style="margin:0px 0.8rem;"><?php the_time(); ?></p>
+            </div>
+            <p class="post-comments"><a style="text-decoration: none;" href="<?php echo comments_link(); ?>"><?php echo get_comments_number();?> comments</a></p>
+          </div>
+          <div class="single_post-content">
+            <p><?php the_excerpt(); ?></p>
+            <a href=" <?php echo get_post_permalink(); ?>">Read more</a>
+          </div>
+       </div>
+     </div>
+      <?php
+    endwhile;
+else:
+    _e( 'Sorry, no posts matched your criteria.', 'wb_domain' );
+endif;
+wp_reset_postdata();
+?>
 
 
 
---
+++ template-parts/blog-posts.php
@@ -0,0 +1,154 @@
+<div class="single_post">
+  <div class="single_post-header">
+    <div class="single_post-date">
+      <p>22</p>
+      <p>DEC</p>
+    </div>
+    <div class="">
+      <span class="vl"></span> Going to the place and making a case!
+    </div>
+  </div>
+  <div class="post-content">
+    <div class="post_img_container">
+      <img class="post-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-2.png">
+    </div>
+    <div class="post_details_container">
+      <div class="single_post-meta">
+        <div class="single_post-meta-author">
+          <h4 style="margin:0px; font-weight:normal;">by <span>Robin Sen</span></h4>
+          <p style="margin:0px 0.8rem;">On 21 Dec 2020</p>
+        </div>
+        <p class="post-comments">12 comments</a></p>
+      </div>
+      <hr class="post-divider">
+      <div class="single_post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim .</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+ </div>
+</div>
+
+<div class="single_post">
+  <div class="single_post-header"  style="	background-color: #ef3313;">
+    <div class="single_post-date">
+      <p>21</p>
+      <p>DEC</p>
+    </div>
+    <div class="">
+      <span class="vl"></span> Achieve your grandest dreams!!
+    </div>
+  </div>
+  <div class="post-content">
+    <div class="post_img_container">
+      <img class="post-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-1.png">
+    </div>
+    <div class="post_details_container">
+      <div class="single_post-meta">
+        <div class="single_post-meta-author">
+          <h4 style="margin:0px; font-weight:normal;">by <span>Robin Sen</span></h4>
+          <p style="margin:0px 0.8rem;">On 21 Dec 2020</p>
+        </div>
+        <p class="post-comments">12 comments</a></p>
+      </div>
+      <hr class="post-divider">
+      <div class="single_post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim .</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+ </div>
+</div>
+
+<div class="single_post">
+  <div class="single_post-header">
+    <div class="single_post-date">
+      <p>19</p>
+      <p>DEC</p>
+    </div>
+    <div class="">
+      <span class="vl"></span> I want to put a ding in the universe!
+    </div>
+  </div>
+  <div class="post-content">
+    <div class="post_img_container">
+      <img class="post-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-3.png">
+    </div>
+    <div class="post_details_container">
+      <div class="single_post-meta">
+        <div class="single_post-meta-author">
+          <h4 style="margin:0px; font-weight:normal;">by <span>Robin Sen</span></h4>
+          <p style="margin:0px 0.8rem;">On 21 Dec 2020</p>
+        </div>
+        <p class="post-comments">12 comments</a></p>
+      </div>
+      <hr class="post-divider">
+      <div class="single_post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim .</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+ </div>
+</div>
+
+<div class="single_post">
+  <div class="single_post-header">
+    <div class="single_post-date">
+      <p>18</p>
+      <p>DEC</p>
+    </div>
+    <div class="">
+      <span class="vl"></span> It is time to pause and reflect..
+    </div>
+  </div>
+  <div class="post-content">
+    <div class="post_img_container">
+      <img class="post-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-6.png">
+    </div>
+    <div class="post_details_container">
+      <div class="single_post-meta">
+        <div class="single_post-meta-author">
+          <h4 style="margin:0px; font-weight:normal;">by <span>Robin Sen</span></h4>
+          <p style="margin:0px 0.8rem;">On 21 Dec 2020</p>
+        </div>
+        <p class="post-comments">12 comments</a></p>
+      </div>
+      <hr class="post-divider">
+      <div class="single_post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim .</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+ </div>
+</div>
+
+<div class="single_post">
+  <div class="single_post-header">
+    <div class="single_post-date">
+      <p>17</p>
+      <p>DEC</p>
+    </div>
+    <div class="">
+      <span class="vl"></span> Blessed is who has found his work.
+    </div>
+  </div>
+  <div class="post-content">
+    <div class="post_img_container">
+      <img class="post-img" src="<?php echo  get_theme_file_uri(); ?>/assets/images/image-5.png">
+    </div>
+    <div class="post_details_container">
+      <div class="single_post-meta">
+        <div class="single_post-meta-author">
+          <h4 style="margin:0px; font-weight:normal;">by <span>Robin Sen</span></h4>
+          <p style="margin:0px 0.8rem;">On 21 Dec 2020</p>
+        </div>
+        <p class="post-comments">12 comments</a></p>
+      </div>
+      <hr class="post-divider">
+      <div class="single_post-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim .</p>
+        <a href="#">Read more</a>
+      </div>
+    </div>
+ </div>
+</div>
