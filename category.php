4f1026816bf0c644e8e38a5ab2ad5cf40e1e9014
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-26 12:37:40 +0530

    edit portfolio img

--- category.php
+++ category.php
@@ -58,9 +58,16 @@ if ( $query -> have_posts() ):
     while ( $query -> have_posts() ):
       $query -> the_post();
       ?>
-  <img class="myImg grid_container-item grid_container-img" src="<?php
-            $image=wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail');
-            echo $image[0];?>">
+  <div class="portfolio-images">
+      <img class="myImg grid_container-item grid_container-img" src="<?php
+      $image=wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail');
+      echo $image[0];?>">
+      <div class="portfolio-images-overlay">
+        <a href="#" class="icon" title="User Profile">
+          <i class="fa fa-eye"></i>
+        </a>
+      </div>
+  </div>
   <?php
   endwhile;
   ?>
